70e08b405cd437fdf66ddd9e761100b4a7b64261
User-Google <81684863+User-Google@users.noreply.github.com>
2021-04-08 12:21:37 +0400

    Add files via upload

---
+++ Novodimitrovskaya2k7/3.php
@@ -0,0 +1,41 @@
+<!-- Через 10 секунд после появления сообщения об отправке или ошибке — отправляемся на сайт Кода :) -->
+<meta http-equiv='refresh' content='2; url=http://novodmitrovskaya2k7.ru/'>
+<!-- Начался блок PHP -->
+<?php 
+
+require_once('phpmailer/PHPMailerAutoload.php');
+$mail = new PHPMailer;
+$mail->CharSet = 'utf-8';
+
+$phone = $_POST['phone'];
+
+//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+
+$mail->isSMTP();                                      // Set mailer to use SMTP
+$mail->Host = 'smtp.mail.ru';  																							// Specify main and backup SMTP servers
+$mail->SMTPAuth = true;                               // Enable SMTP authentication
+$mail->Username = 'mail_sander_228@mail.ru'; // Ваш логин от почты с которой будут отправляться письма
+$mail->Password = '$dk820&123'; // Ваш пароль от почты с которой будут отправляться письма
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+
+$mail->setFrom('mail_sander_228@mail.ru'); // от кого будет уходить письмо?
+$mail->addAddress('bryanskaya.elena@century21.ru');     // Кому будет уходить письмо 
+//$mail->addAddress('ellen@example.com');               // Name is optional
+//$mail->addReplyTo('info@example.com', 'Information');
+//$mail->addCC('cc@example.com');
+//$mail->addBCC('bcc@example.com');
+//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+$mail->isHTML(true);                                  // Set email format to HTML
+
+$mail->Subject = 'Москва, Новодмитровская улица, 2к7';
+$mail->Body    = 'Имя пользователь не оставил'  . '<br>Телефон: ' .$phone;
+$mail->AltBody = '';
+
+if(!$mail->send()) {
+    echo 'Error';
+} else {
+    header('location: thank-you.html');
+}
+?>
